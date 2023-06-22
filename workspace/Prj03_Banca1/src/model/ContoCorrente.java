f9a14b581066837659bebc7df6d7bffdd53bb366
maboglia <mauro.bogliaccino@gmail.com>
2023-06-22 12:48:59 +0200

    aggiornamento

--- workspace/Prj03_Banca1/src/model/ContoCorrente.java
+++ workspace/Prj03_Banca1/src/model/ContoCorrente.java
@@ -2,33 +2,41 @@ package model;
 
 public class ContoCorrente {
 
+	private static int contatore = 1;
+	
+	private int numero;
 	public Cliente titolare;
 	private double saldo;
-	
+
 	public ContoCorrente(Cliente titolare) {
+		this.numero = contatore++;
 		this.titolare = titolare;
 		this.saldo = 0;
 	}
-	
+
 	public void versamento(double sommaVersata) {
 		this.saldo += sommaVersata;
 	}
-	
+
 	public double prelievo(double sommaRichiesta) {
-		
+
 		if (sommaRichiesta < this.saldo) {
 			this.saldo -= sommaRichiesta;
 			return sommaRichiesta;
 		}
-		System.out.println("Non puoi ritirare " + sommaRichiesta );
+		System.out.println("Non puoi ritirare " + sommaRichiesta);
 		System.out.println("La disponibilità sul conto è di " + this.saldo);
 		return 0;
-		
+
 	}
-	
-	public double getSaldo() {//metodo getter
+
+	public double getSaldo() {// metodo getter
 		return this.saldo;
 	}
-	
-	
+
+	public String report() {
+		return "Conto corrente n. " + this.numero 
+				+ "\n" + "titolare: " + this.titolare.nome
+				+ "\n" + "saldo: " + this.saldo;
+	}
 }
--- workspace/Prj03_Banca1/src/test/ContoCorrenteTest.java
+++ workspace/Prj03_Banca1/src/test/ContoCorrenteTest.java
@@ -7,18 +7,23 @@ public class ContoCorrenteTest {
 
 	public static void main(String[] args) {
 		
-		Cliente c1 = new Cliente("mauro");
+		Cliente c1 = new Cliente("gennaro");
 		
 		ContoCorrente cc1 = new ContoCorrente(c1);
+		ContoCorrente cc2 = new ContoCorrente(c1);
 		
 		cc1.versamento(1000);
+		cc2.versamento(2000);
+		
 		
 		cc1.prelievo(500);
 		
 		cc1.prelievo(250);
 		
-		System.out.println(cc1.getSaldo());
+		cc2.prelievo(500);
 		
+		System.out.println(cc1.report());
+		System.out.println(cc2.report());
 
 	}
 
